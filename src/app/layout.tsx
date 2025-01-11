28aeefcd59053bade3041372a1f60e84dd8b9571
Ajayaditya-Codes <ajayaditya.dev@proton.me>
2025-01-11 08:19:55 +0530

    vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { ThemeProvider } from "@/providers/theme-provider";
 import { AuthProvider } from "@/providers/AuthProvider";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata: Metadata = {
   title: "Quirk",
@@ -17,7 +19,8 @@ export default function RootLayout({
     <AuthProvider>
       <html lang="en" suppressHydrationWarning>
         <body>
-          {" "}
+          <Analytics />
+          <SpeedInsights />
           <ThemeProvider
             attribute="class"
             defaultTheme="system"
